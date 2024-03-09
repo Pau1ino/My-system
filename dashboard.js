dfe414e5ff4df6cae5c7edebb23c6fd87a9a2635
Pau1ino <162384583+Pau1ino@users.noreply.github.com>
2024-03-08 20:25:00 -0600

    Rough version of journal

--- dashboard.js
+++ dashboard.js
@@ -13,15 +13,39 @@ btn.addEventListener('click', function () {
 })
 
 let entry = document.getElementById("entry");
-let journalCard = document.getElementById("journal-section");
-let journalBody = document.getElementById("journal-body");
-//We want to grab the input inside the event so it's not empty
-entry.addEventListener("click", function(){
-    let journalInput = document.getElementById("journal").value;
-    let text = document.createElement("p");
-    text.textContent = journalInput;
 
-    journalBody.appendChild(text);
+let journalBody = document.getElementById("journal-body");
+
+//We want to grab the input inside the event so it's not empty
+let card = document.createElement("div");
+
+entry.addEventListener("click", function(){
+    let journalInput = document.getElementById("journal").value; //J input
+
+
+    // let text = document.createElement("p");//Create p
+
+
+    // text.textContent = journalInput; //p = Journal input
+    let journalT = document.getElementById("journalT").value;  //Journal title
+
+    let entro = document.createElement("p");
+    
+    let entroid = journalT + journalInput;
+
+        console.log(journalT);
+
+    entro.textContent += entroid;
+
+    
+
+    //Make card
+    let card = document.createElement("div");
+    card.appendChild(entro);
+    // card.appendChild(text);
+
+    // //Append card
+    journalBody.appendChild(card);
 })
 
 /* 
@@ -29,4 +53,16 @@ entry.addEventListener("click", function(){
 2. Grabb the text
 3. Create a p element and attatch the text to it
 4. On the journal body, go ahead and add the inputted text
+*/
+
+
+
+/* 
+<div>
+    <div>
+        <div>
+        <div>
+    <div>
+<duv>
+
 */
