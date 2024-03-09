1d9832f8c695be37c14a07d957bad9d65c7ee92c
Pau1ino <162384583+Pau1ino@users.noreply.github.com>
2024-03-08 19:37:39 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -11,3 +11,22 @@ btn.addEventListener('click', function () {
         input.value = "";
     } else {alert("You need to input a goal first")};
 })
+
+let entry = document.getElementById("entry");
+let journalCard = document.getElementById("journal-section");
+let journalBody = document.getElementById("journal-body");
+//We want to grab the input inside the event so it's not empty
+entry.addEventListener("click", function(){
+    let journalInput = document.getElementById("journal").value;
+    let text = document.createElement("p");
+    text.textContent = journalInput;
+
+    journalBody.appendChild(text);
+})
+
+/* 
+1. Once the button is clicked run an event that
+2. Grabb the text
+3. Create a p element and attatch the text to it
+4. On the journal body, go ahead and add the inputted text
+*/
