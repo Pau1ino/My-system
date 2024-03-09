b6af4e85f5ae717bebfa00ef74ef894605f8763c
Pau1ino <162384583+Pau1ino@users.noreply.github.com>
2024-03-08 18:49:05 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,12 @@
 let btn = document.getElementById('btn');
-let input = document.getElementById('input');
 let ul = document.getElementById("goalUl");
 
 btn.addEventListener('click', function () {
-    if(input.value.length > 0){
+    let input = document.getElementById('input').value;
+  
+    if(input.length > 0){
         let li = document.createElement("li");
-        li.textContent = input.value;
+        li.textContent = input;
         ul.appendChild(li);
         input.value = "";
     } else {alert("You need to input a goal first")};
