a9d8452b9cf8450a7c3e4fd0308c00af43f218d1
Pau1ino <162384583+Pau1ino@users.noreply.github.com>
2024-03-09 11:13:03 -0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,68 +1,45 @@
 let btn = document.getElementById('btn');
 let ul = document.getElementById("goalUl");
+let entry = document.getElementById("entry");
+let journalBody = document.getElementById("journal-body");
+
 
 btn.addEventListener('click', function () {
     let input = document.getElementById('input').value;
-  
-    if(input.length > 0){
+
+    if (input.length > 0) {
         let li = document.createElement("li");
         li.textContent = input;
         ul.appendChild(li);
-        input.value = "";
-    } else {alert("You need to input a goal first")};
+        document.getElementById("input").value = "";
+    } else { alert("You need to input a goal first") };
 })
-
-let entry = document.getElementById("entry");
-
-let journalBody = document.getElementById("journal-body");
 
 //We want to grab the input inside the event so it's not empty
-let card = document.createElement("div");
+entry.addEventListener("click", function () {
+    let journalInput = document.getElementById("journal").value; //Journal Input Value
+    let journalTitle = document.getElementById("journalT").value;  //Journal title
 
-entry.addEventListener("click", function(){
-    let journalInput = document.getElementById("journal").value; //J input
+    if (journalInput.length && journalTitle.length > 0) {
+        //Journal Content
+        let p = document.createElement("p");
+        p.textContent = journalInput;  //Create Paragraph and add the jounral input to it
 
+        //Journal Title
+        let h3 = document.createElement("h3");
+        h3.textContent = journalTitle;
 
-    // let text = document.createElement("p");//Create p
+        //Div to append to parent
+        let card = document.createElement("div");
+        card.appendChild(h3);
+        card.appendChild(p);
 
+        //Append card to the parent
+        journalBody.appendChild(card);
 
-    // text.textContent = journalInput; //p = Journal input
-    let journalT = document.getElementById("journalT").value;  //Journal title
+        //Now lets clean up the inputs
+        document.getElementById("journal").value = "";
+        document.getElementById("journalT").value = "";
+    } else { alert("You need to have a title and some content before inputting") };
 
-    let entro = document.createElement("p");
-    
-    let entroid = journalT + journalInput;
-
-        console.log(journalT);
-
-    entro.textContent += entroid;
-
-    
-
-    //Make card
-    let card = document.createElement("div");
-    card.appendChild(entro);
-    // card.appendChild(text);
-
-    // //Append card
-    journalBody.appendChild(card);
 })
-
-/* 
-1. Once the button is clicked run an event that
-2. Grabb the text
-3. Create a p element and attatch the text to it
-4. On the journal body, go ahead and add the inputted text
-*/
-
-
-
-/* 
-<div>
-    <div>
-        <div>
-        <div>
-    <div>
-<duv>
-
-*/
